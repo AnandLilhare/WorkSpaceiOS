04fd1accdf654cff302039c696ee54589ef7b028
Anand Lilhare <anandlilhare69@gmail.com>
2021-09-23 11:12:33 +0530

    completed chapter 9

---
+++ Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    let storyArray = [
+       
+
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        storyLabel.text = "You see a fork in the road."
+        choice1Button.setTitle("Take a left.", for: .normal)
+        choice2Button.setTitle("Take a right.", for: .normal)
+    }
+
+
+}
+
---
+++ Quizzler-iOS13-master/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    var quizBrain = QuizBrain()
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var questionLable: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUi()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        
+        if quizBrain.checkAnswer(userAnswer) {
+            sender.backgroundColor = UIColor.green
+        }else {
+            sender.backgroundColor = UIColor.red
+        }
+        quizBrain.nextQuestion()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUi), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUi() {
+        questionLable.text = quizBrain.getQuizText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        progressBar.progress = quizBrain.getProgressCount()
+    }
+}
+
